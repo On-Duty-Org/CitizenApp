25816439664af57e515c3b77e8d4ea4c2e53b11e
Ayush Ujjwal <ujjwal.msrit@gmail.com>
2020-07-15 12:56:25 +0530

    added the first screen containing work pattern

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+const kWidgetNormalText = TextStyle(
+  fontSize: 20,
+  letterSpacing: 1.8,
+);
+
+const kHeadingText =
+    TextStyle(fontWeight: FontWeight.w600, fontSize: 30, letterSpacing: 1.8);
